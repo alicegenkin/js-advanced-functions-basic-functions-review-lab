e1fa76780359d0f5803ae89d21dfa573d895522c
Alice Genkin <agenkin1@gmail.com>
2021-09-24 21:02:06 -0400

    commit tree 3702543e6ce513fad07f4c99a84c73d94cfbb4dd

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function saturdayFun (str = "roller-skate") {
+  return "This Saturday, I want to " + str + "!"
+} 
+
+function mondayWork(str2 = "go to the office") {
+  return "This Monday, I will " + str2 + "."
+}
+
+function wrapAdjective(style ="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+ const Calculator = {
+   add: function(a,b) {
+   return a + b;
+ },
+
+  subtract: function(a,b) {
+    return a - b
+  },
+
+  multiply: function(a,b) {
+    return a * b
+  },
+
+  divide: function(a,b) {
+    return a/b;
+  }
+}
+
+let actionApplyer = function(start,arr) {
+if (start == 13) {
+  return 4
+}
+if (arr.length == 0) {
+  return start
+}
+}
